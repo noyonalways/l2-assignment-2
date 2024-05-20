b8fc1a44e973b085a4fb1a3f748e9d73065dd5a2
noyonalways <noyonrahman2003@gmail.com>
2024-05-20 21:01:41 +0600

    feat: global error handler adde with 404 route

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,4 +1,5 @@
-import { Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
+import { TCustomError } from "../types";
 const router: Router = Router();
 
 router.get("/", (_req: Request, res: Response) => {
@@ -10,5 +11,27 @@ router.get("/", (_req: Request, res: Response) => {
 router.get("/health", (_req: Request, res: Response) => {
   res.status(200).send("OK");
 });
+
+router.use((_req: Request, _res: Response, next: NextFunction) => {
+  const err: TCustomError = new Error("Route not found");
+  err.status = 404;
+  err.success = false;
+
+  next(err);
+});
+
+router.use(
+  (
+    err: TCustomError,
+    _req: Request,
+    res: Response,
+    _next: NextFunction,
+  ): void => {
+    res.status(err.status || 500).json({
+      success: err.success,
+      message: err.message,
+    });
+  },
+);
 
 export default router;
